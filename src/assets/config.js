7f371beb2517c50c7269594c14a52764bfdcc199
Lalo <siedutunja@galeanoti.com>
2026-07-21 00:58:11 -0500

    1.1.5 Correción error

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,13 +1,13 @@
 //*** Api Localhost
-//export const ROOT_PATH = "http://localhost:5001/api/"
-//export const ROOT_HOME = "http://localhost:8084/loginacudientes/"
-//export const ROOT_MODULO_ACUDIENTES = "http://localhost:8085/acudientes/"
-//export const ID_INSTITUCION = ""
+export const ROOT_PATH = "http://localhost:5001/api/"
+export const ROOT_HOME = "http://localhost:8084/loginacudientes/"
+export const ROOT_MODULO_ACUDIENTES = "http://localhost:8085/acudientes/"
 
 //*** Api Servidor
-export const ROOT_PATH = "https://siedutunja.gov.co:3001/api/"
-export const ROOT_HOME = "https://siedutunja.gov.co/loginacudientes/"
-export const ROOT_MODULO_ACUDIENTES = "https://siedutunja.gov.co/acudientes"
+//export const ROOT_PATH = "https://siedutunja.gov.co:3001/api/"
+//export const ROOT_HOME = "https://siedutunja.gov.co/loginacudientes/"
+//export const ROOT_MODULO_ACUDIENTES = "https://siedutunja.gov.co/acudientes"
+
 export const ID_INSTITUCION = ""
 
 export const ROOT_WEBSITE = "https://siedutunja.gov.co"
